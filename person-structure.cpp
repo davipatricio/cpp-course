cf6b05de8ebc126e2191bc23e23fdc4da4f44191
davipatricio <davipatricio2006@gmail.com>
2022-05-08 15:26:41 -0300

    add person-strucutre exercise

---
+++ person-structure.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+typedef struct Person {
+    char nome[100];
+    int idade;
+} Person;
+
+int main() {
+    Person p[2];
+    strcpy(p[0].nome, "Davi");
+    p[0].idade = 16;
+
+    strcpy(p[1].nome, "Joao");
+    p[1].idade = 18;
+
+    cout << "Nome da primeira pessoa: " << p[0].nome << endl;
+    cout << "Idade da primeira pessoa: " << p[0].idade << endl << endl;
+
+    cout << "Nome da segunda pessoa: " << p[1].nome << endl;
+    cout << "Idade da segunda pessoa: " << p[1].idade << endl;
+
+    return 0;
+}
