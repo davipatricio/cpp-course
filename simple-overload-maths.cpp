e8075f9837807b8e04d3fb498d6cdcee6d22eb32
davipatricio <davipatricio2006@gmail.com>
2022-05-08 12:33:43 -0300

    add simple-overload-maths exercise

---
+++ simple-overload-maths.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int add(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+int main()
+{
+    cout << add(5, 5) << endl;
+    cout << add(5, 5, 5) << endl;
+    return 0;
+}
